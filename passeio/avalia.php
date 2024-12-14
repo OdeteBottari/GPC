c497ed49a5ac9fe95ae0e736fb18a09c691dfed3
Kety041 <ketelyn.c@estudante.ifmt.edu.br>
2024-12-14 09:23:31 -0400

    Add files via upload

--- passeio/avalia.php
+++ passeio/avalia.php
@@ -7,6 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     // Exemplo de resposta:
     echo "<h1>Avaliação Recebida!</h1>";
+    echo "<p>nome: $nome</p>";
     echo "<p>Nota: $rating</p>";
     echo "<p>Comentário: $comment</p>";
 } else {
